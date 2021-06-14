dce846ca76d5a3e05de9df3a6cb9b6209653a11d
chrislovecnm <clove@cnmconsulting.net>
2021-06-14 13:57:34 -0600

    removing incorrect files

--- yaml/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-HOST=$(ctlptl get cluster kind-kind -o template --template '{{.status.localRegistryHosting.host}}')
-cat deployment.yaml | sed "s/REGISTRY_HOST_PLACEHOLDER/$HOST/" | kubectl apply -f -
